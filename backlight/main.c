bbac3c9dfb455d61489ae181e0059fa26292752e
Martin <m.wuerms@msr.ch>
2015-11-05 20:35:36 +0100

    added project backlight

---
+++ backlight/main.c
@@ -0,0 +1,178 @@
+/**
+ * Martin Wuerms
+ * 2015-09-13
+ *
+ * attiny85 pin description
+ * name     pin     functions (attiny85)
+ * PB0      5       (MOSI/DI/SDA/AIN0/OC0A/OC1A/AREF/PCINT0)
+ * PB1      6       (MISO/DO/AIN1/OC0B/OC1A/PCINT1)
+ * PB2      7       (SCK/USCK/SCL/ADC1/T0/INT0/PCINT2)
+ * PB3      2       (PCINT3/XTAL1/CLKI/OC1B/ADC3)
+ * PB4      3       (PCINT4/XTAL2/CLKO/OC1B/ADC2)
+ * PB5      1       (PCINT5/RESET/ADC0/dW)
+ * VCC      8       2.7 ... 5.5 V
+ * GND      4
+ *
+ * pin usage on pcb
+ * name     signal
+ * PB3      USB D+
+ * PB4      USB D-
+ * PB5      Reset
+ */
+
+/* - includes --------------------------------------------------------------- */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <string.h>
+#include "arch.h"
+#include "wdtTimer.h"
+#include "ledDriver.h"
+
+/* - typedefs --------------------------------------------------------------- */
+
+/* - defines ---------------------------------------------------------------- */
+#define fEV_UPDATE_LEDs     _BV(0));
+
+#define cNB_LEDs            (10)
+#define cBRIGHTNESS_0       (0)
+#define cBRIGHTNESS_1       (100)
+#define cBRIGHTNESS_2       (200)
+#define cBRIGHTNESS_STEP    (1)
+
+/* - variables -------------------------------------------------------------- */
+volatile uint8_t gloabl_events, local_events;
+rgb_color_t leds[cNB_LEDs];
+uint8_t led_state, led_timeout;
+
+/* - private functions  ----------------------------------------------------- */
+/**
+ * enter the given sleep mode, see avr/sleep.h
+ * @param   mode    sleep modes:
+ * translate: own defines to arv/sleep.h:
+ * cSLEEP_MODE_... | SLEEP_MODE_...
+ * cSLEEP_MODE_ACTIVE | none, do not enter this function
+ * cSLEEP_MODE_IDLE | SLEEP_MODE_IDLE
+ *  none | SLEEP_MODE_ADC
+ * cSLEEP_MODE_POWER_DOWN | SLEEP_MODE_PWR_DOWN
+ *  none | SLEEP_MODE_PWR_SAVE
+ *  none | SLEEP_MODE_STANDBY
+ *  none | SLEEP_MODE_EXT_STANDBY
+ */
+static void _EnterSleepMode(uint8_t mode) {
+    set_sleep_mode(mode);
+    sleep_enable();
+    sei();
+    sleep_cpu();    // enter sleep mode, wait here
+    sleep_disable();
+    cli();
+}
+
+/**
+ * set all red leds to a given value
+ */
+static void _SetAllRedLEDs(uint8_t value) {
+    uint8_t i;
+    for(i = 0; i < cNB_LEDs; i++) {
+        leds[i].red = value;
+    }
+}
+
+/**
+ * process the leds
+ */
+static void leds_Update(void) {
+    uint8_t i;
+    switch(led_state) {
+        case 0:
+            led_state = 1;
+            led_timeout = 6;
+            _SetAllRedLEDs(cBRIGHTNESS_0);
+            break;
+        case 1:
+            led_state = 2;
+            led_timeout = 3;
+            _SetAllRedLEDs(cBRIGHTNESS_1);
+            break;
+        case 2:
+            led_state = 3;
+            led_timeout = 6;
+            _SetAllRedLEDs(cBRIGHTNESS_0);
+            break;
+        case 3:
+            led_state = 4;
+            led_timeout = 3;
+            _SetAllRedLEDs(cBRIGHTNESS_1);
+            break;
+        case 4:
+            led_state = 5;
+            led_timeout = 60;
+            _SetAllRedLEDs(cBRIGHTNESS_0);
+            break;
+
+        case 5:
+            led_timeout = 3;
+            if(leds[0].red < cBRIGHTNESS_2) {
+                _SetAllRedLEDs(leds[0].red + cBRIGHTNESS_STEP);
+            }
+            else {
+                led_state = 6;
+            }
+            break;
+        case 6:
+            led_timeout = 3;
+            if(leds[0].red > cBRIGHTNESS_1) {
+                _SetAllRedLEDs(leds[0].red - cBRIGHTNESS_STEP);
+            }
+            else {
+                led_state = 5;
+            }
+            break;
+
+        default: led_state = 0;
+    }
+
+}
+/**
+ * initialize all components
+ */
+static void init(void) {
+    cli();
+
+    gloabl_events = 0;
+    local_events = fEV_UPDATE_LEDs;
+    led_state = 0;
+    memset(leds, 0, sizeof(*leds));
+
+    wdtTimer_Init(cEV_TIMER_INTERVAL_64MS);
+
+    sei();
+}
+
+/* - public functions ------------------------------------------------------- */
+
+/**
+ * main loop
+ */
+int main (void)
+{
+    init();
+    while(1) {
+        if(local_events & fEV_UPDATE_LEDs) {
+            leds_Update();
+            ledDriver_Set(leds, cNB_LEDs, 1);
+            wdtTimer_StartTimeout(led_timeout, fEV_UPDATE_LEDs);
+        }
+
+        while(1) {
+            cli();
+            local_events = gloabl_events;
+            gloabl_events = 0;
+            if(local_events) {
+                sei();
+            }
+            _EnterSleepMode(SLEEP_MODE_PWR_DOWN);
+        }
+    }
+    return(0);
+}
---
+++ backlight/wdtTimer.c
@@ -0,0 +1,65 @@
+/**
+ * Martin Wuerms
+ * 2015-10-18
+ *
+ */
+
+/* - includes --------------------------------------------------------------- */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "arch.h"
+#include "wdtTimer.h"
+
+/* - defines ---------------------------------------------------------------- */
+
+/* - typedef ---------------------------------------------------------------- */
+
+/* - variables -------------------------------------------------------------- */
+static volatile uint8_t wdt_interval, wdt_timeout, wdt_event_flag;
+
+/* - private functions ------------------------------------------------------ */
+
+/**
+ * stop wdtTimer
+ */
+static inline void wdtTimer_Stop(void) {
+    WDTCR = (1 << WDCE);
+    WDTCR = 0;
+}
+
+/**
+ * watchdog ISR
+ */
+ISR(WDT_vect) {
+    if(wdt_timeout > 0) {
+        wdt_timeout--;
+    }
+    else {
+        wdtTimer_Stop();
+        // gloabl_events |= wdt_event_flag;
+    }
+}
+
+/* - public functions ------------------------------------------------------- */
+
+/**
+ * initialize the event timer
+ */
+void wdtTimer_Init(uint8_t interval) {
+    wdt_interval = interval & 0x07;
+}
+
+/**
+ * start the wdtTimer
+ * @param   timeout     overall timeout = interval * timeout
+ */
+void wdtTimer_StartTimeout(uint8_t timeout, uint8_t event_flag) {
+    uint8_t sr;
+    lock_interrupt(sr);
+    wdt_timeout = timeout;
+    wdt_event_flag = event_flag;
+    WDTCR = (1 << WDCE) | (1 << WDIF);
+    WDTCR = (wdt_interval);
+    WDTCR |= (1 << WDIE);
+    restore_interrupt(sr);
+}
